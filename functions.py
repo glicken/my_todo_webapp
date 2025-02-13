61da31542fcb382441df14014c2f6689757777e1
Nicholas Smith <glicken@gmail.com>
2025-02-13 00:35:46 +0000

    fixed new filepath now that files are in same directory

--- functions.py
+++ functions.py
@@ -1,17 +1,18 @@
 # FILEPATH = "/Users/nicholas/Python_Projects/Udemy_Nov_2024/Exercises/Main_app/todos.txt"
-FILEPATH = "/Users/nicholas/Test Workspace/Todo_Web_App/todos.txt"
+#FILEPATH = "/Users/nicholas/Test Workspace/Todo_Web_App/todos.txt"
 
 
-def get_todos(filepath=FILEPATH):
+
+def get_todos():
     ''' Read a text file and return the todo items'''
-    #filepath = "/Users/nicholas/Python_Projects/Udemy_Nov_2024/Exercises/Main_app/todos.txt"
-    with open(filepath, "r", encoding="utf-8") as file_local:
+    #todos.txt" = "/Users/nicholas/Python_Projects/Udemy_Nov_2024/Exercises/Main_app/todos.txt"
+    with open("todos.txt", "r", encoding="utf-8") as file_local:
         todos_local = file_local.readlines()
         return todos_local
 
-def write_todos(todos_arg, filepath=FILEPATH):
-    #filepath = "/Users/nicholas/Python_Projects/Udemy_Nov_2024/Exercises/Main_app/todos.txt"
-    with open(filepath, "w", encoding="utf-8") as file:
+def write_todos(todos_arg):
+    #todos.txt" = "/Users/nicholas/Python_Projects/Udemy_Nov_2024/Exercises/Main_app/todos.txt"
+    with open("todos.txt", "w", encoding="utf-8") as file:
         file.writelines(todos_arg)
         
         
